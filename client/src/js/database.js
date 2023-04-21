edb27e9428faff886cd87eb54fd5a735da574cf0
Cyril1743 <75587967+Cyril1743@users.noreply.github.com>
2023-04-20 21:13:04 -0500

    Set up webpack config and asset caching

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,21 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+//Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1)
+  const transaction = jateDb.transaction("jate", 'readwrite')
+  const objStore = transaction.objectStore('jate')
+  await objStore.add({text: content})
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+//Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const jateDb = await openDB("jate", 1)
+  const transaction = jateDb.transaction("jate", "readonly")
+  const objStore = transaction.objectStore('jate')
+  const request = await objStore.getAll()
+  return request
+}
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,9 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
+//Add and configure workbox plugins for a service worker and manifest file.
+//Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -13,17 +14,51 @@ module.exports = () => {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack"
+      }),
+      new InjectManifest({
+        swSrc: "src-sw.js",
+        swDest: "service-worker.js"
+      }),
+      new WebpackPwaManifest({
+        name: 'Text Editor',
+        short_name: "Text Editor",
+        start_url: "./",
+        icons: [
+          {
+            src: path.join(__dirname, "/src/images/logo.png"),
+            sizes: [96, 128, 256, 384, 512],
+            purpose: ["any", "maskable"]
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /^\.css$/i,
+          use: ['style-loader', 'css-loader']
+        }, {
+          test: /^\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/transform-runtime']
+            }
+          }
+        }
       ],
     },
   };
